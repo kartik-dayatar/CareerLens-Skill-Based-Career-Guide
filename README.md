0624bc39f310d4fac7747d6281ca6d26d1051fff
kartik-dayatar <kartikdayatar25@gmail.com>
2026-01-05 00:05:49 +0530

    Fix git identity for new account

---
+++ README.md
@@ -0,0 +1,1 @@
+chnaged my git account 
--- backend/app/main.py
+++ backend/app/main.py
@@ -4,4 +4,5 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "CareerLens backend running"}
+    return {"message": "CareerLens backend running perfectly and setup is complete"}
+
